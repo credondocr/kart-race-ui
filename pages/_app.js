e3f9301dc75232df18b87947c602d95b898d5d67
Cesar Redondo <credondocr@gmail.com>
2024-10-21 22:17:58 -0600

    Implementing page with details

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,21 @@ import '../styles/globals.css'
 import '../styles/leaderboard.css'; 
 import '../styles/race-table.css'; 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="app-container">
+      {/* Renderizamos la página */}
+      <div className="content">
+        <Component {...pageProps} />
+      </div>
+
+      {/* Footer */}
+      <footer className="footer">
+        <p>
+          Con amor ❤️ al kartismo - Cesar Redondo - <a href="mailto:credondocr@gmail.com">credondocr@gmail.com</a>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default MyApp
